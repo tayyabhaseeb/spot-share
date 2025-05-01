5ebccba00d704074aeff4ffcfb1d27e40c900915
“Tayyab <tayyabhaseeb5@gmail.com>
2025-05-01 14:52:02 +0100

    basic_structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World APP component</h1>;
+    </div>
+  );
+}
